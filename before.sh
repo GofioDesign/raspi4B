fc0443fa449c231824f4cb45b503f12f8d9e8230
Gofio Design <74048325+GofioDesign@users.noreply.github.com>
2023-04-12 22:28:09 +0000

    some UX

--- before.sh
+++ before.sh
@@ -1,6 +1,4 @@
 ## installed 64 bit version of raspberry pi OS no desktop
 sudo apt update && sudo apt upgrade
 sudo apt install git
-git clone https://github.com/GofioDesign/raspi4B.git
-cd raspi4B
-sudo sh install.sh
+git clone https://github.com/GofioDesign/raspi4B.git && sudo sh raspi4B/install.sh
--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo apt install locales-all
 sudo locale-gen es_ES.UTF-8
 sudo update-locale LANG=es_ES.UTF-8
 sudo apt update && sudo apt upgrade
+echo "run sudo sh install2.sh after reboot"
+sleep(5)
 sudo reboot
-#after reboot run install2.sh
