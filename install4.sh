d90d24b9aeb92ce6be9df3d912c93ccf9cf5accb
Gofio Design <74048325+GofioDesign@users.noreply.github.com>
2023-04-12 23:34:24 +0000

    corrected syntax docker compose -f folder up -d

--- install4.sh
+++ install4.sh
@@ -3,11 +3,12 @@ docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 ## install nginx proxy manager
-docker compose up -d -f /nmp/docker-compose.yml
+docker compose -f /nmp/docker-compose.yml up -d
 
 ## install ddclien behind npm
-docker compose up -d -f /ddclient/docker-compose.yml
+docker compose -f /ddclient/docker-compose.yml up -d
 
 git clone https://github.com/GofioDesign/wordpress-nginx-docker-armv7.git
 mv wordpress-nginx-docker-armv7/ wp1/
-docker compose up -d -f /wp1/docker-compose.yml
+docker compose -f /wp1/docker-compose.yml up -d
+
