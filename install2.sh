3dbea5b2354f466082f1e8dfaa94c47514178b29
Gofio Design <74048325+GofioDesign@users.noreply.github.com>
2023-04-12 22:44:30 +0000

    install 2 ux

--- install2.sh
+++ install2.sh
@@ -1,4 +1,11 @@
 # locale # check locale was succesful
+echo "trying distro upgrade"
+sleep 2
 sudo apt dist-upgrade
 sudo apt full-upgrade
+echo "trying bootloader upgrade"
+sleep 2
 sudo rpi-eeprom-update
+echo "going for docker"
+sudo sh install3.sh
+
